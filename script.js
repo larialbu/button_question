822291893a8b79a176869e2a77c3469125106b61
Larissa Albuquerque <105246799+larialbu@users.noreply.github.com>
2023-05-09 00:01:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+$("#change").click(function() {
+    $("#change").html("Sim");
+    $("#move").show();
+});
+
+$("#change").click(function() {
+    $("#message").show();
+});
+
+$("#move").hover(function() {
+    change();
+});
+
+function change() {
+    var i = Math.floor(Math.random() * 500) + 1;
+    var j = Math.floor(Math.random() * 100) + 1;
+    $("#move").css({
+        top: j+'px',
+        left: i+'px'
+    });
+}
